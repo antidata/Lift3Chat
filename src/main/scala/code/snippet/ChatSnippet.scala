67f9dda24a6ade9eb3bc892328fe78c631aa22ad
antidata <antidata.05@gmail.com>
2015-10-10 12:25:08 -0500

    ChatSnippet

---
+++ src/main/scala/code/snippet/ChatSnippet.scala
@@ -0,0 +1,38 @@
+package code.snippet
+
+import code.managers.{Message, ChatManager}
+import net.liftweb.common.Logger
+import net.liftweb.http.js.JE.JsRaw
+import net.liftweb.http.{RoundTripInfo, S, RoundTripHandlerFunc}
+import net.liftweb.json.JsonAST.{JString, JValue}
+import scala.xml.NodeSeq
+
+class ChatSnippet extends Logger {
+  def render() : NodeSeq = {
+    val funcs: List[RoundTripInfo] = List("sendMessage" -> sendMessage _, "getMessages" -> sendMessage _)
+
+    for {
+      session <- S.session
+    } {
+      S.appendJs(JsRaw(s"var pageFunctions = ${session.buildRoundtrip(funcs).toJsCmd}").cmd)
+    }
+    NodeSeq.Empty
+  }
+
+  def getMessages(value : JValue, func: RoundTripHandlerFunc): Unit = {
+    ChatManager.getMessages(func)
+  }
+
+  def sendMessage(value : JValue, func: RoundTripHandlerFunc): Unit = {
+    import net.liftweb.json.JsonDSL._
+
+    (value \ "name", value \ "text") match {
+      case (JString(name), JString(text)) =>
+        _logger.debug(s"Getting $text from $name")
+        ChatManager.newMessage(Message(name, text))
+
+      case _ =>
+        _logger.debug("Error")
+    }
+  }
+}
